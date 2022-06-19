13353d706a39e3f0bc1418eafdc3abb922b70646
mharunrasyid <rasyidharunar04@gmail.com>
2022-06-19 17:34:20 +0700

    remove console

--- rasyidfilm/helpers/util.js
+++ rasyidfilm/helpers/util.js
@@ -2,7 +2,6 @@ var jwt = require("jsonwebtoken");
 
 module.exports = {
   isLoggedIn: function (req, res, next) {
-    console.log(req.headers.authorization);
       let token = req.headers.authorization.replace("Bearer ", "");
       jwt.verify(token, "shhhhh", function (err, decoded) {
         if (err) return res.status(401).json(err);
